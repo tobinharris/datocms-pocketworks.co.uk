b6fefd5da534fddf0ef1a0e2a8128447f9597907
Tobin Harris <tobin@pocketworks.co.uk>
2020-01-15 22:40:56 +0000

    Got a hero workin on home page.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
+    { 
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        includePaths: ["./src"],
+      }
+    },
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-datocms`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Masonry from 'react-masonry-component'
+import "../styles/index.sass";
+import "../styles/app.sass";
+import {Section, Container, Navbar, Brand} from 'react-bulma-components';
 import Img from 'gatsby-image'
-import Layout from "../components/layout"
+
+
+//import Layout from "../components/layout"
 
 const IndexPage = ({ data }) => (
-  <Layout>
+<div>
+  <Navbar>
+    <Navbar.Brand>
+      <Navbar.Item renderAs="a" href="/">
+          <img src="https://www.datocms-assets.com/20955/1579120310-secondary-logo-small-v3.png"></img>
+      </Navbar.Item>      
+    </Navbar.Brand>
+  </Navbar>
+  <Section size="medium" className="hero">
+    <Container>
+    <div class="columns">
+      <div class="column is-5 is-offset-1">
+        <h1 class="title is-size-1">We develop mobile apps to make light work of business.</h1>
+        <p>
+          <a class="button is-primary is-rounded is-large">Tell me more</a>&nbsp;&nbsp;
+          <Link to="/about" className="button is-info is-rounded is-outlined is-large">Contact us</Link>
+        </p>
+        <p><br/>Or learn more <a href="/about">about us</a>.</p>
+      </div>
+      <div class="column is-4 is-offset-1">
+        <img src="https://pocketworks.co.uk/wp-content/uploads/2019/08/Hero-Image-Padding-e1566211830226.png" style={{position: 'relative',top: '-4rem'}} />
+        
+      </div>
+    </div>
+    </Container>
+  </Section>
     <Masonry className="showcase">
       {data.allDatoCmsWork.edges.map(({ node: work }) => (
         <div key={work.id} className="showcase__item">
@@ -25,7 +55,7 @@ const IndexPage = ({ data }) => (
         </div>
       ))}
     </Masonry>
-  </Layout>
+</div>
 )
 
 export default IndexPage
