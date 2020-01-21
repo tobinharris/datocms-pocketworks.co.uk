aa00961eb639675e81a5dab99688e85a97fb2fad
Tobin Harris <tobin@pocketworks.co.uk>
2020-01-21 09:14:25 +0000

    Blog showing 18 cards, nicer title.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,7 +4,7 @@ import "../styles/index.sass";
 import "../styles/app.sass";
 import Layout from '../components/layout'
 import BlogCard from '../components/blog-card'
-import {Section, Container, Media, Card, Hero, Columns} from 'react-bulma-components';
+import { Section, Container, Media, Card, Hero, Columns } from 'react-bulma-components';
 import Img from 'gatsby-image'
 import '@fortawesome/fontawesome-free/css/all.css'
 import SiteNav from '../components/navbar'
@@ -12,51 +12,54 @@ import SiteNav from '../components/navbar'
 //import Layout from "../components/layout"
 
 const BlogPage = ({ data }) => (
-<Layout>
-  <SiteNav></SiteNav>
-  <div className="tabs is-medium is-centered">
+  <Layout>
+    <SiteNav></SiteNav>
+    <div className="tabs is-medium is-centered">
       <ul>
-          <li className="is-active"><a>Latest</a></li>          
-          <li><Link to="#">Strategy</Link></li>
-          <li><Link to="#">Design</Link></li>
-          <li><Link to="#">Technology</Link></li>
-          <li><Link to="#">Us</Link></li>
+        <li className="is-active"><a>Latest</a></li>
+        {/* <li><Link to="#">Strategy</Link></li>
+        <li><Link to="#">Design</Link></li>
+        <li><Link to="#">Technology</Link></li>
+        <li><Link to="#">Us</Link></li> */}
       </ul>
-  </div>
-  <Section className="hero">
-    <Hero.Body>
-      <Container>
-      <div className="columns content">
-        <div className="column is-6 is-offset-1">
-          
-          <h2 className="title is-size-3-mobile">
-          Our <span class="has-text-primary">blog</span> about recent events at Pocketworks and in the digital tech industry.
-          </h2>
-            
-          
-         
-        </div>
-        <div className="column is-5">
-          <img src="https://www.datocms-assets.com/20955/1579120467-speedupillustrationpocketworks.png?fm=jpg&w=250" />          
-        </div>
-      </div>
-      <Columns>
-        <Columns.Column className="is-offset-1 is-10">
-            <Columns className="is-centered">
-                {data.posts.edges.map(({ node: article }) => (      
-                <Columns.Column className="is-4 is-multiline">
-                    <BlogCard article={article}></BlogCard>
-                </Columns.Column>
-                ))}
-            </Columns>
-        </Columns.Column>      
-      </Columns>
+    </div>
+    <Hero className=" is-small">
+      <Hero.Body>
+        <Container>
+          <div className="columns content">
+            <div className="column is-6 is-offset-1">
 
-      
+              <h1 className="title is-size-1 is-size-3-mobile">
+                Recent <span class="has-text-primary">news &amp; thoughts</span> about mobile.
+          </h1>
+
+
+
+            </div>
+            <div className="column is-5">
+              <img src="https://www.datocms-assets.com/20955/1579120516-blog.png?fm=jpg&w=350" />
+            </div>
+          </div>
+        </Container>
+      </Hero.Body>
+    </Hero>
+    <Section>
+      <Container>
+        <Columns>
+          <Columns.Column className="is-offset-1 is-10">
+            <Columns className="is-centered">
+              {data.posts.edges.map(({ node: article }) => (
+                <Columns.Column className="is-4 is-multiline">
+                  <BlogCard article={article}></BlogCard>
+                </Columns.Column>
+              ))}
+            </Columns>
+          </Columns.Column>
+        </Columns>
       </Container>
-    </Hero.Body>
-  </Section>  
-</Layout>
+    </Section>
+  </Layout>
+
 
 )
 
@@ -64,7 +67,7 @@ export default BlogPage
 
 export const query = graphql`
   query BlogQuery {
-    posts: allDatoCmsArticle(limit: 16, sort: {fields: [date], order: DESC}) {
+    posts: allDatoCmsArticle(limit: 18, sort: {fields: [date], order: DESC}) {
       edges {
         node {
           id
