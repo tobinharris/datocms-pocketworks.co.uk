7e9541ec7aaffe1077ad080ff75c9358cb6cadef
Tobin Harris <tobin@pocketworks.co.uk>
2020-01-25 22:41:00 +0000

    Burger working

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "../styles/app.sass";
 
 import { Link, graphql } from 'gatsby'
@@ -22,36 +22,62 @@ import Img from 'gatsby-image'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 //import Layout from "../components/layout"
+class SiteNav extends React.Component {
+  componentDidMount() {
+    //<i class="fas fa-bars"></i>
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-function SiteNav(props){
-  return (<Navbar className="is-spaced">
-    <Navbar.Brand>      
-      <Navbar.Item renderAs="span">
-      <Link to="/">
-        {props.className === 'is-light' ? (
-        <img src={props.fullLogo || true ? "https://www.datocms-assets.com/20955/1579120308-secondary-logo-white-v3.png": "https://www.datocms-assets.com/20955/1579431602-secondary-logo-small-square-white-v3a.png"} alt="Pocketworks Logo"/>       
-        ) : (
-        <img src={props.fullLogo || true ? "https://www.datocms-assets.com/20955/1579120310-secondary-logo-small-v3.png" : "https://www.datocms-assets.com/20955/1579431570-secondary-logo-small-square-v3a.png"} alt="Pocketworks Logo"/>       
-        )}
-      </Link>           
-      </Navbar.Item>   
-      <Navbar.Burger data-target="site-menu">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </Navbar.Burger>      
-    </Navbar.Brand>
-    
-    <div id="site-menu" className="navbar-menu navbar-end">
-      
-        <Navbar.Item className={props.className} renderAs="span"><Link to="/work" activeClassName="is-active" partiallyActive={true}>Work</Link></Navbar.Item>
-        <Navbar.Item className={props.className} renderAs="span"><Link to="/about/story" activeClassName="is-active" partiallyActive={true}>About</Link></Navbar.Item>
-        <Navbar.Item className={props.className} renderAs="span"><Link to="/blog" activeClassName="is-active" partiallyActive={true}>Blog</Link></Navbar.Item>
-        <Navbar.Item className={props.className} renderAs="span"><Link to="/contact" activeClassName="is-active" partiallyActive={true}>Contact</Link></Navbar.Item>        
-      
-    </div>
-  </Navbar>
-  )
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+      // Add a click event on each of them
+      $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+
+        });
+      });
+    }
+  }
+  render() {
+    return (
+      <Navbar className={"is-spaced" + this.props.className}>
+        <Navbar.Brand>
+          <Navbar.Item renderAs="span">
+            <Link to="/">
+              {this.props.className === 'is-light' ? (
+                <img src={this.props.fullLogo || true ? "https://www.datocms-assets.com/20955/1579120308-secondary-logo-white-v3.png" : "https://www.datocms-assets.com/20955/1579431602-secondary-logo-small-square-white-v3a.png"} alt="Pocketworks Logo" />
+              ) : (
+                  <img src={this.props.fullLogo || true ? "https://www.datocms-assets.com/20955/1579120310-secondary-logo-small-v3.png" : "https://www.datocms-assets.com/20955/1579431570-secondary-logo-small-square-v3a.png"} alt="Pocketworks Logo" />
+                )}
+            </Link>
+          </Navbar.Item>
+          <Navbar.Burger className="navbar-burner" data-target="site-menu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>            
+          </Navbar.Burger>
+        </Navbar.Brand>
+
+        <div id="site-menu" className="navbar-menu navbar-end">
+
+          <Navbar.Item className={this.props.className} renderAs="span"><Link to="/work" activeClassName="is-active" partiallyActive={true}>Work</Link></Navbar.Item>
+          <Navbar.Item className={this.props.className} renderAs="span"><Link to="/about/story" activeClassName="is-active" partiallyActive={true}>About</Link></Navbar.Item>
+          <Navbar.Item className={this.props.className} renderAs="span"><Link to="/blog" activeClassName="is-active" partiallyActive={true}>Blog</Link></Navbar.Item>
+          <Navbar.Item className={this.props.className} renderAs="span"><Link to="/contact" activeClassName="is-active" partiallyActive={true}>Contact</Link></Navbar.Item>
+
+        </div>
+      </Navbar>
+    )
+  }
 }
 
 export default SiteNav;
